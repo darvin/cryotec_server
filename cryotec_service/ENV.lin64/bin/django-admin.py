5f34e7a72b0a794c482bc892c74a436ae5e907df
darvin <darvin@darvin-laptop.(none)>
2010-08-16 16:54:27 +0300

    ENV64

---
+++ cryotec_service/ENV.lin64/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/darvin/ENV.lin64/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.2.1','django-admin.py'
+__requires__ = 'Django==1.2.1'
+import pkg_resources
+pkg_resources.run_script('Django==1.2.1', 'django-admin.py')
