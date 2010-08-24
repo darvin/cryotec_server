eed6636cb601cadc7bda0d0947eb78a33453d327
darvin <darvin@darvin-laptop.(none)>
2010-08-24 14:29:15 +0300

    contact face

--- cryotec_service/clients/admin.py
+++ cryotec_service/clients/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from clients.models import Client
+from clients.models import Client, ContactFace
 from machines.admin import MachineClientInline,\
     MachineCustomerInline
 from files.admin import UploadInline
+from libs.admin import LinkedInline
+
+
+
+class ContactFaceInline(LinkedInline):
+    model = ContactFace
 
 
 class ClientAdmin(admin.ModelAdmin):
-    inlines = [MachineClientInline, MachineCustomerInline]
+    inlines = [MachineClientInline, MachineCustomerInline, ContactFaceInline]
     inlines += [UploadInline]
 
 
