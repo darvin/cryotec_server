81608516ef2c99b5a2c67f401db29a3b40ddb169
Sergey Klimov <dcdarv@gmail.com>
2010-10-21 08:22:08 +0300

    buildout

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+          name = "cryotec_server",
+          version = "1.0",
+          url = 'http://github.com/darvin/cryotec_server',
+          license = 'Greedy Open Source',
+          description = "Cryotec Service Server",
+          author = 'Sergey Klimov',
+          packages = find_packages('src'),
+          package_dir = {'': 'src'},
+          install_requires = ['setuptools'],
+     )
